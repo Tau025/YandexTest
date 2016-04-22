91616534ce4a26e779672993bee3d4b4860b890d
tau <d29025@yandex.ru>
2016-04-22 15:53:43 +0300

    changed buggy compileSdkVersion 23 to 22, created and applied styles for TextViews, adjusted precise layout params to fit screenshots

--- app/src/main/java/tau/yandextest/Singer.java
+++ app/src/main/java/tau/yandextest/Singer.java
@@ -16,13 +16,22 @@ public class Singer {
     public static ArrayList<Singer> singers = new ArrayList<>();
     static {
         singers.add(new Singer(1, "Bonobo", new String[]{"dance", "electronics"}, 8, 95, R.drawable.bonobo,
-                "long biography of Bonobo"));
+                "Simon Green (born 30 March 1976), known by his stage name Bonobo, is a British musician, producer and DJ based in Los Angeles. " +
+                        "Green has recorded and performed solo DJ sets under the name Barakas, and together with Robert Luis " +
+                        "from Tru Thoughts as Nirobi and Barakas."));
         singers.add(new Singer(2, "Tycho", new String[]{"dance", "electronics"}, 6, 70, R.drawable.tycho,
-                "long biography of Tycho"));
+                "Tycho (pronounced like Tie-ko, with emphasis on the first syllable) is an American ambient music project " +
+                        "led by Scott Hansen (born 1976 or 1977) as primary composer, songwriter and producer. " +
+                        "Hailing from San Francisco, California, he is known as ISO50 for his photographic and design works."));
         singers.add(new Singer(3, "Odesza", new String[]{"dance", "electronics"}, 5, 24, R.drawable.odesza,
-                "long biography of Odesza"));
+                "Odesza (stylized as ODESZA) are an American electronic music duo from Seattle consisting of Harrison Mills " +
+                        "(CatacombKid) and Clayton Knight (BeachesBeaches). The group was formed in 2012 shortly before " +
+                        "Mills and Knight graduated from Western Washington University."));
         singers.add(new Singer(4, "Ásgeir", new String[]{"dance", "electronics"}, 5, 44, R.drawable.asgeir,
-                "long biography of Ásgeir"));
+                "Ásgeir's debut album is Dýrð í dauðaþögn released in 2012, the lead single from which, \"Sumargestur\", " +
+                        "made it to number two on the Tónlist,[3] an unofficial but widely quoted Icelandic Singles Chart, " +
+                        "followed up with the single \"Leyndarmál\"[4] (six weeks at number 1 on Tónlist)[3] and the title track " +
+                        "\"Dýrð í dauðaþögn\" from the album (three weeks at #1 on Tónlist)."));
     }
 
     public Singer(int id, String name, String[] genres, int albumsQty, int tracksQty, int imageResourceId, String biography) {
