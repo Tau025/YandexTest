5c2a04c46c78216ae2bbe1a6385302e0f764386d
tau <d29025@yandex.ru>
2016-04-22 16:40:51 +0300

    infoImage in DetailsActivity now keeps aspect ratio on all devices

--- app/src/main/java/tau/yandextest/DetailsActivity.java
+++ app/src/main/java/tau/yandextest/DetailsActivity.java
@@ -7,7 +7,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.Locale;
 /**
@@ -15,6 +18,8 @@ import java.util.Locale;
  */
 public class DetailsActivity extends AppCompatActivity {
     public static final String EXTRA_ID = "extra_id";
+    private static final String LOG_TAG = "DetailsActivity";
+    private int INFO_IMAGE_HEIGHT = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,11 +36,19 @@ public class DetailsActivity extends AppCompatActivity {
         Resources res = getResources();
         Locale locale = res.getConfiguration().locale;
 
-        ImageView imageView = (ImageView) findViewById(R.id.info_image);
+        //prepare ImageView
+        ImageView iv_infoImage = (ImageView) findViewById(R.id.info_image);
         Drawable drawable = ContextCompat.getDrawable(this, singer.getImageResourceId());
-        imageView.setImageDrawable(drawable);
-        imageView.setContentDescription(singer.getName());
+        iv_infoImage.setImageDrawable(drawable);
+        iv_infoImage.setContentDescription(singer.getName());
+        measureScreen();
+        Log.d(LOG_TAG, "INFO_IMAGE_HEIGHT: " + String.valueOf(INFO_IMAGE_HEIGHT));
+        iv_infoImage.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                INFO_IMAGE_HEIGHT
+        ));
 
+        //prepare TextView for singer genres
         TextView tv_genres = (TextView) findViewById(R.id.genres);
         StringBuilder genres = new StringBuilder();
         String prefix = "";
@@ -46,12 +59,22 @@ public class DetailsActivity extends AppCompatActivity {
         }
         tv_genres.setText(genres);
 
+        //prepare TextView for quantity of singer's albums and tracks
         TextView tv_albumsAndTracksQty = (TextView) findViewById(R.id.albums_and_tracks_qty);
         String albums = res.getQuantityString(R.plurals.count_of_albums, singer.getAlbumsQty(), singer.getAlbumsQty());
         String tracks = res.getQuantityString(R.plurals.count_of_tracks, singer.getTracksQty(), singer.getTracksQty());
         tv_albumsAndTracksQty.setText(String.format(locale, "%s  ·  %s", albums, tracks));
 
+        //prepare TextView for Singer's biography
         TextView tv_biographyText = (TextView) findViewById(R.id.biography_text);
         tv_biographyText.setText(singer.getBiography());
+    }
+
+    //is used to keep aspect ratio of infoImage
+    private void measureScreen() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int screenWidth = metrics.widthPixels;
+        INFO_IMAGE_HEIGHT = Math.round(screenWidth / 384 * 245);
     }
 }
